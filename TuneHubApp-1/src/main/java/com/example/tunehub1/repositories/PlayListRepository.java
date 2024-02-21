5b3495fb4795e01e6f54e66ed9808c58598f8b7e
Mahesh <147246762+MaheshKakarla2003@users.noreply.github.com>
2024-02-21 19:28:29 +0530

    Delete TuneHubApp-1 directory

--- TuneHubApp-1/src/main/java/com/example/tunehub1/repositories/PlayListRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.tunehub1.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.tunehub1.entities.PlayList;
-
-public interface PlayListRepository extends JpaRepository<PlayList, Integer> {
-
-}
--- TuneHubApp-1/src/main/java/com/example/tunehub1/services/PlayListService.java
+++
@@ -1,7 +0,0 @@
-package com.example.tunehub1.services;
-
-public interface PlayListService {
-
-	
-
-}
